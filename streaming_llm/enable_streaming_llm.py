d5d8f2090b234dcc32e842638fc47d8968bef928
leonardozcm <leonardo1997zcm@gmail.com>
2023-10-25 12:40:55 +0800

    initial commit on spr

--- streaming_llm/enable_streaming_llm.py
+++ streaming_llm/enable_streaming_llm.py
@@ -1,10 +1,10 @@
-from streaming_llm.kv_cache import StartRecentKVCache
+from kv_cache import StartRecentKVCache
 
 
 def enable_streaming_llm(model, start_size, recent_size):
     if "llama" in model.config.model_type:
         k_seq_dim = v_seq_dim = 2
-        from streaming_llm.pos_shift.modify_llama import (
+        from pos_shift.modify_llama import (
             enable_llama_pos_shift_attention,
         )
 
@@ -14,7 +14,7 @@ def enable_streaming_llm(model, start_size, recent_size):
         k_seq_dim = 3
     elif "gpt_neox" in model.config.model_type:
         k_seq_dim = v_seq_dim = 2
-        from streaming_llm.pos_shift.modify_gpt_neox import (
+        from pos_shift.modify_gpt_neox import (
             enable_gpt_neox_pos_shift_attention,
         )
 
@@ -22,7 +22,7 @@ def enable_streaming_llm(model, start_size, recent_size):
     elif "falcon" in model.config.model_type:
         v_seq_dim = 1
         k_seq_dim = 1
-        from streaming_llm.pos_shift.modify_falcon import (
+        from pos_shift.modify_falcon import (
             enable_falcon_pos_shift_attention,
         )
 
